005a1159d58bce124bd0c3a314b12c4115ee373a
Stozo <35351828+Stozo@users.noreply.github.com>
2024-10-15 21:57:29 +0200

    Oppgave 2 Ferdig

---
+++ Oppgave 2.py
@@ -0,0 +1,12 @@
+import random
+
+# 1 Definer en funksjon som lager en fin utskrift med et tilfeldig generert tall mellom 0 og 100
+# 2 Funksjonen skal ikke ta noen parametere.
+def rng():
+    print("Ditt tilfeldige tall mellom 0 og 100 er:",random.randint(0,100))
+
+# 3 Kall denne funksjonen noen ganger.
+rng()
+rng()
+rng()
+rng()
